798ea3fd865b5241ce2122d6efac3b5e14b99137
Yrimu-Wan-code <patiencegachie@gmail.com>
2026-04-18 17:54:39 +0300

    Weather alerts App

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-// index.js
-const weatherApi = "https://api.weather.gov/alerts/active?area="
+function appSetup() {
+    const input = document.querySelector("#state-input");
+    const submitButton = document.querySelector("#fetch-alerts");
+    const displaySection = document.querySelector("#alerts-display");
+    const errorMessage = document.querySelector("#error-message");
 
-// Your code here!
+    if(!input || !submitButton || !displaySection || !errorMessage) {
+        return;
+    }
+    async function getWeatherAlerts() {
+        try {
+            displaySection.innerHTML = "";
+            errorMessage.textContent = "";
+            errorMessage.classList.add("hidden");
+            const state = input.value.trim().toUpperCase();
+
+            const response = await fetch(`https://api.weather.gov/alerts/active?area=${state}`);
+            
+            const data = await response.json();
+            const alerts = data.features;
+
+            const summaryMessage = document.createElement("p");
+            summaryMessage.textContent = `Weather Alerts: ${alerts.length}`;
+            displaySection.appendChild(summaryMessage);
+
+            alerts.forEach(alert => {
+                const alertMessage = document.createElement("p");
+                alertMessage.textContent = alert.properties.headline;
+                displaySection.appendChild(alertMessage);
+            });
+                
+            input.value = "";
+        } catch (error) {errorMessage.textContent = "Error fetching weather alerts. Please try again.";
+            errorMessage.classList.remove("hidden");
+        }
+    }
+    submitButton.addEventListener("click", getWeatherAlerts);
+
+    document.addEventListener("DOMContentLoaded",appSetup);
+}
